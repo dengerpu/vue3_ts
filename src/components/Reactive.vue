f5e1cfa964f17a4eaa7c8a2af9196fa38193e337
dep <1649743146@qq.com>
2023-10-03 20:05:28 +0800

    feat: 新增ref和reactive注释

--- src/components/Reactive.vue
+++ src/components/Reactive.vue
@@ -14,17 +14,27 @@ import { defineComponent, reactive } from 'vue'
 export default defineComponent({
   name: 'ReactiveV',
   setup () {
-    const proxy = reactive({
+    /*
+    reactive
+        作用: 定义多个数据的响应式
+        const proxy = reactive(obj): 接收一个普通对象然后返回该普通对象的响应式代理器对象
+        响应式转换是“深层的”：会影响对象内部所有嵌套的属性
+        内部基于 ES6 的 Proxy 实现，通过代理对象操作源对象内部数据都是响应式的
+    */
+    const obj = {
       name: '张三',
       car: {
         name: '奔驰',
         color: 'red'
       }
-    })
+    }
+    const proxy = reactive(obj)
     function update () {
       proxy.name += '=='
       proxy.car.name += '=='
     }
+    //  总结: 如果操作代理对象,目标对象中的数据也会随之变化,
+    // 同时如果想要在操作数据的时候,界面也要跟着重新更新渲染,那么也是操作代理对象
     return {
       proxy,
       update
--- src/components/Ref.vue
+++ src/components/Ref.vue
@@ -16,6 +16,8 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'RefV',
   setup () {
+    // ref是一个函数,作用:定义一个响应式的数据,返回的是一个Ref对象,对象中有一个value属性,如果需要对数据进行操作,
+    // 需要使用该Ref对象调用value属性的方式进行数据的操作
     const count = ref(1)
     const obj = ref({
       name: '张三',
